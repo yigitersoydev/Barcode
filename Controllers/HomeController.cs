6e54d04ea6677ff3782073d12fc0a2ce91fcff35
Yiğit <Yiit@YIGIT>
2023-05-30 23:45:52 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,128 @@
+﻿using BarcodeGenerator.Models;
+using IronBarCode;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace BarcodeGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private readonly IWebHostEnvironment _environment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult CreateBarcode()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBarcode(GenerateBarcodeModel generateBarcode, string radioButton)
+        {
+            long first12Digit, last11Digit;
+            string barcodeNum;
+            int digit1_2 = Convert.ToByte(generateBarcode.Tip);
+            int digit3_4 = 06, digit5_6 = 16, digit7 = 3;
+            int digit8 = Convert.ToByte(generateBarcode.SogumaSuresi);
+            int digit12 = 8;
+            int digit13_14 = generateBarcode.Voltaj;
+            float digit15_16_17 = generateBarcode.Direnc;
+            int digit18 = generateBarcode.Rezistans;
+            int digit19_20_21;
+            int digit22 = generateBarcode.EnerjiDuzeltmeEksi;
+            int digit23 = generateBarcode.EnerjiDuzeltmeArti;
+            try
+            {
+                if (radioButton == "Saniye")
+                {
+                    digit19_20_21 = generateBarcode.Sure;
+                    if (generateBarcode.Cap2 != null)
+                    {
+                        int digit9_10_11_calc = generateBarcode.CalculateDiameter(Convert.ToInt16(generateBarcode.Cap1), Convert.ToInt16(generateBarcode.Cap2));
+                        first12Digit = Convert.ToInt64(string.Format("{0}{1}{2}{3}{4}{5}{6}", digit1_2, digit3_4.ToString("D2"), digit5_6, digit7, digit8, digit9_10_11_calc.ToString("D3"), digit12));
+                        last11Digit = Convert.ToInt64(string.Format("{0}{1}{2}{3}{4}{5}", digit13_14, digit15_16_17, digit18, digit19_20_21.ToString("D3"), digit22, digit23));
+                        int digit24 = Convert.ToInt32(generateBarcode.GenerateDigit24(first12Digit, last11Digit));
+                        barcodeNum = first12Digit.ToString() + last11Digit.ToString() + digit24.ToString();
+                    }
+                    else
+                    {
+                        int digit9_10_11 = Convert.ToInt16(generateBarcode.Cap1);
+                        first12Digit = Convert.ToInt64(string.Format("{0}{1}{2}{3}{4}{5}{6}", digit1_2, digit3_4.ToString("D2"), digit5_6, digit7, digit8, digit9_10_11.ToString("D3"), digit12));
+                        last11Digit = Convert.ToInt64(string.Format("{0}{1}{2}{3}{4}{5}", digit13_14, digit15_16_17, digit18, digit19_20_21.ToString("D3"), digit22, digit23));
+                        int digit24 = Convert.ToInt32(generateBarcode.GenerateDigit24(first12Digit, last11Digit));
+                        barcodeNum = first12Digit.ToString() + last11Digit.ToString() + digit24.ToString();
+                    }
+                    GeneratedBarcode barcode = IronBarCode.BarcodeWriter.CreateBarcode(barcodeNum, BarcodeWriterEncoding.ITF);
+                    barcode.ResizeTo(500, 120);
+                    barcode.AddBarcodeValueTextBelowBarcode();
+                    barcode.ChangeBarCodeColor(System.Drawing.Color.Black);
+                    barcode.SetMargins(10);
+                    string path = Path.Combine(_environment.WebRootPath, "GeneratedBarcode");
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    string filePath = Path.Combine(_environment.WebRootPath, "GeneratedBarcode/barcode.png");
+                    barcode.SaveAsPng(filePath);
+                    string fileName = Path.GetFileName(filePath);
+                    string imageUrl = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}" + "/GeneratedBarcode/" + fileName;
+                    ViewBag.QrCodeUri = imageUrl;
+                }
+                else if (radioButton == "Dakika")
+                {
+                    digit19_20_21 = generateBarcode.ModifyNumber(generateBarcode.Sure);
+                    if (generateBarcode.Cap2 != null)
+                    {
+                        int digit9_10_11_calc = generateBarcode.CalculateDiameter(Convert.ToInt16(generateBarcode.Cap1), Convert.ToInt16(generateBarcode.Cap2));
+                        first12Digit = Convert.ToInt64(string.Format("{0}{1}{2}{3}{4}{5}{6}", digit1_2, digit3_4.ToString("D2"), digit5_6, digit7, digit8, digit9_10_11_calc.ToString("D3"), digit12));
+                        last11Digit = Convert.ToInt64(string.Format("{0}{1}{2}{3}{4}{5}", digit13_14, digit15_16_17, digit18, digit19_20_21.ToString("D3"), digit22, digit23));
+                        int digit24 = Convert.ToInt32(generateBarcode.GenerateDigit24(first12Digit, last11Digit));
+                        barcodeNum = first12Digit.ToString() + last11Digit.ToString() + digit24.ToString();
+                    }
+                    else
+                    {
+                        int digit9_10_11 = Convert.ToInt16(generateBarcode.Cap1);
+                        first12Digit = Convert.ToInt64(string.Format("{0}{1}{2}{3}{4}{5}{6}", digit1_2, digit3_4.ToString("D2"), digit5_6, digit7, digit8, digit9_10_11.ToString("D3"), digit12));
+                        last11Digit = Convert.ToInt64(string.Format("{0}{1}{2}{3}{4}{5}", digit13_14, digit15_16_17, digit18, digit19_20_21.ToString("D3"), digit22, digit23));
+                        int digit24 = Convert.ToInt32(generateBarcode.GenerateDigit24(first12Digit, last11Digit));
+                        barcodeNum = first12Digit.ToString() + last11Digit.ToString() + digit24.ToString();
+                    }
+                    GeneratedBarcode barcode = IronBarCode.BarcodeWriter.CreateBarcode(barcodeNum, BarcodeWriterEncoding.ITF);
+                    barcode.ResizeTo(500, 120);
+                    barcode.AddBarcodeValueTextBelowBarcode();
+                    barcode.ChangeBarCodeColor(System.Drawing.Color.Black);
+                    barcode.SetMargins(10);
+                    string path = Path.Combine(_environment.WebRootPath, "GeneratedBarcode");
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    string filePath = Path.Combine(_environment.WebRootPath, "GeneratedBarcode/barcode.png");
+                    barcode.SaveAsPng(filePath);
+                    string fileName = Path.GetFileName(filePath);
+                    string imageUrl = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}" + "/GeneratedBarcode/" + fileName;
+                    ViewBag.QrCodeUri = imageUrl;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/GenerateBarcodeModel.cs
@@ -0,0 +1,222 @@
+﻿using IronBarCode;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace BarcodeGenerator.Models
+{
+    public class GenerateBarcodeModel
+    {
+        public Tip Tip { get; set; }
+        public int MyProperty { get; set; }
+        public Cap1 Cap1 { get; set; }
+        public Cap2? Cap2 { get; set; }
+        public byte Voltaj { get; set; }
+        public float Direnc { get; set; }
+        public int Sure { get; set; }
+        public SogumaSuresi SogumaSuresi { get; set; }
+        public byte Rezistans { get; set; }
+        public byte EnerjiDuzeltmeEksi { get; set; }
+        public byte EnerjiDuzeltmeArti { get; set; }
+
+        public int CalculateDiameter(int D1, int D2)
+        {
+            int[] arrD = { 020, 025, 032, 040, 050, 063, 075, 090, 110, 125, 140, 160, 180, 200, 225, 250, 315 };
+            int d1 = Array.IndexOf(arrD, D1);
+            int d2 = Array.IndexOf(arrD, D2);
+            int d = (d1 * 31) + d2;
+            return d;
+        }
+        public int ModifyNumber(int number)
+        {
+            int firstDigit = 9;
+            int secondDigit;
+            int modifiedNumber;
+            if (number > 99)
+            {
+                secondDigit = (number / 10);
+                modifiedNumber = (firstDigit * 100) + secondDigit;
+                return modifiedNumber;
+            }
+            else
+            {
+                secondDigit = number / 10;
+                int remainingDigits = number % 10;
+                if (remainingDigits != 0)
+                    modifiedNumber = (firstDigit * 100) + (secondDigit * 10) + remainingDigits;
+                else{ modifiedNumber = (firstDigit * 100) + number; }
+                return modifiedNumber;
+            }
+
+        }
+        public int GenerateDigit24(long first, long last)
+        {
+            int lastSumEven, lastSumOdd, digit24;
+            int sumEvenFirst, sumOddFirst, sumEvenLast, sumOddLast;
+
+            sumEvenFirst = Convert.ToInt32(sumEven(first));
+            sumOddFirst = Convert.ToInt32(sumOdd(first));
+            sumEvenLast = Convert.ToInt32(sumEven(last));
+            sumOddLast = Convert.ToInt32(sumOdd(last));
+            lastSumEven = sumEvenFirst + sumEvenLast;
+            lastSumOdd = sumOddFirst + sumOddLast;
+            lastSumOdd = lastSumOdd * 3;
+            digit24 = lastSumOdd + lastSumEven;
+            digit24 = 10 - (digit24 % 10);
+            if (digit24 == 10)
+                return 0;
+            return digit24;
+        }
+        public long reverse(long barcode)
+        {
+            long rev = 0;
+            while (barcode != 0)
+            {
+                rev = (rev * 10) + (barcode % 10);
+                barcode /= 10;
+            }
+            return rev;
+        }
+        public long sumOdd(long barcode)
+        {
+            barcode = reverse(barcode);
+            long sumOdd = 0, c = 1;
+            while (barcode != 0)
+            {
+                if (c % 2 != 0)
+                    sumOdd += barcode % 10;
+                barcode /= 10;
+                c++;
+            }
+            return sumOdd;
+        }
+        public long sumEven(long barcode)
+        {
+            barcode = reverse(barcode);
+            long sumEven = 0, c = 1;
+            while (barcode != 0)
+            {
+                if (c % 2 == 0)
+                    sumEven += barcode % 10;
+                barcode /= 10;
+                c++;
+            }
+            return sumEven;
+        }
+    }
+
+    public enum Tip : byte
+    {
+        [Display(Name = "Raytrans")]
+        Raytrans = 91,
+        [Display(Name = "JT.D.W.tapping tee")]
+        JTDW_Tapping_Tee = 92,
+        [Display(Name = "Y Reduction")]
+        Y_Reduction = 93,
+        [Display(Name = "tapping tee")]
+        Tapping_Tee = 94,
+        [Display(Name = "I Coupler")]
+        Coupler = 95,
+        [Display(Name = "[ Single socket")]
+        Single_Socket = 96,
+        [Display(Name = "T Tees")]
+        T_Tees = 97,
+        [Display(Name = "Elbow")]
+        Elbow = 98
+    }
+    public enum Cap1
+    {
+        [Display(Name = "020")]
+        Yirmi = 020,
+        [Display(Name = "025")]
+        YirmiBeş = 025,
+        [Display(Name = "032")]
+        Otuzİki = 032,
+        [Display(Name = "040")]
+        Kırk = 040,
+        [Display(Name = "050")]
+        Elli = 050,
+        [Display(Name = "063")]
+        AltmışÜç = 063,
+        [Display(Name = "075")]
+        YetmişBeş = 075,
+        [Display(Name = "090")]
+        Doksan = 090,
+        [Display(Name = "110")]
+        YüzOn = 110,
+        [Display(Name = "125")]
+        YüzYirmiBeş = 125,
+        [Display(Name = "140")]
+        YüzKırk = 140,
+        [Display(Name = "160")]
+        YüzAltmış = 160,
+        [Display(Name = "180")]
+        YüzSeksen = 180,
+        [Display(Name = "200")]
+        İkiYüz = 200,
+        [Display(Name = "225")]
+        İkiYüzYirmiBeş = 225,
+        [Display(Name = "250")]
+        İkiYüzElli = 250,
+        [Display(Name = "315")]
+        ÜçYüzOnBeş = 315
+    }
+    public enum Cap2
+    {
+        [Display(Name = "020")]
+        Yirmi = 020,
+        [Display(Name = "025")]
+        YirmiBeş = 025,
+        [Display(Name = "032")]
+        Otuzİki = 032,
+        [Display(Name = "040")]
+        Kırk = 040,
+        [Display(Name = "050")]
+        Elli = 050,
+        [Display(Name = "063")]
+        AltmışÜç = 063,
+        [Display(Name = "075")]
+        YetmişBeş = 075,
+        [Display(Name = "090")]
+        Doksan = 090,
+        [Display(Name = "110")]
+        YüzOn = 110,
+        [Display(Name = "125")]
+        YüzYirmiBeş = 125,
+        [Display(Name = "140")]
+        YüzKırk = 140,
+        [Display(Name = "160")]
+        YüzAltmış = 160,
+        [Display(Name = "180")]
+        YüzSeksen = 180,
+        [Display(Name = "200")]
+        İkiYüz = 200,
+        [Display(Name = "225")]
+        İkiYüzYirmiBeş = 225,
+        [Display(Name = "250")]
+        İkiYüzElli = 250,
+        [Display(Name = "315")]
+        ÜçYüzOnBeş = 315
+    }
+    public enum SogumaSuresi : byte
+    {
+        [Display(Name = "5")]
+        Bes,
+        [Display(Name = "10")]
+        On,
+        [Display(Name = "15")]
+        OnBeş,
+        [Display(Name = "20")]
+        Yirmi,
+        [Display(Name = "30")]
+        Otuz,
+        [Display(Name = "45")]
+        KırkBeş,
+        [Display(Name = "60")]
+        Altmış,
+        [Display(Name = "75")]
+        YetmişBeş,
+        [Display(Name = "90")]
+        Doksan,
+    }
+}
